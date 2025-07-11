e2b36661762df3f052a3783734d6c3791eaad700
acer <acer@user.lan>
2025-07-11 13:15:27 +0530

    Initial commit

---
+++ samplemaven/src/main/java/encapsulation/Encapsulation2.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class Encapsulation2 {
+
+	public static void main(String[] args) {
+		Encapsulation1 obj1=new Encapsulation1();
+		obj1.setter("sameesha",27);
+		obj1.getter();
+	}
+
+}
